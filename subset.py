63eb49e5d58b93c17ac38166ccd799571c779a5d
LemonFace0309 <charles.liu0309@gmail.com>
2024-01-15 14:58:09 -0500

    adjusted subset.py to work with proper image dimensions

--- subset.py
+++ subset.py
@@ -11,9 +11,17 @@ with h5py.File('./stimulus/nsd_stimuli.hdf5', 'r') as source_hdf5:
 
     # Loop through and copy the first 1000 images from the source
     for i in range(num_images):
-        source_image = source_hdf5['/imgBrick'][i, :, :, :]
+        # Read the ith image (Adjust indices for 0-based Python indexing)
+        image_data = source_hdf5['imgBrick'][..., i-1]
+
+        # Permute the dimensions to match MATLAB's format
+        image_data = np.transpose(image_data, (2, 1, 0))
+
         # Convert the image to uint8 and store it
-        images[i] = source_image.astype('uint8')
+        images[i] = image_data.astype('uint8')
+
+        # Add to the list of images
+        images.append(image_data)
 
 # Create a dictionary to store the data
 mat_data = {'coco_file': images}
