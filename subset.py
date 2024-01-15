c58d92d81880e284b0dd286484b51db36208eadb
Charles Liu <charles.liu0309@gmail.com>
2024-01-15 15:02:06 -0500

    nit updates

--- subset.py
+++ subset.py
@@ -4,7 +4,7 @@ from scipy.io import savemat
 
 with h5py.File('./stimulus/nsd_stimuli.hdf5', 'r') as source_hdf5:
     # Define the number of images to save
-    num_images = 1000
+    num_images = 100
 
     # Initialize an array to store the images
     images = np.zeros((num_images, 425, 425, 3), dtype='uint8')
@@ -12,13 +12,10 @@ with h5py.File('./stimulus/nsd_stimuli.hdf5', 'r') as source_hdf5:
     # Loop through and copy the first 1000 images from the source
     for i in range(num_images):
         # Read the ith image (Adjust indices for 0-based Python indexing)
-        image_data = source_hdf5['imgBrick'][..., i-1]
+        image_data = source_hdf5['imgBrick'][..., i]
 
         # Permute the dimensions to match MATLAB's format
         image_data = np.transpose(image_data, (2, 1, 0))
-
-        # Convert the image to uint8 and store it
-        images[i] = image_data.astype('uint8')
 
         # Add to the list of images
         images.append(image_data)
