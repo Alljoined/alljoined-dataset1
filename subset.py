17d7da995f469c91167e374033d22ffa0b45b72c
Charles Liu <charles.liu0309@gmail.com>
2024-01-15 23:03:13 -0500

    changed indices

--- subset.py
+++ subset.py
@@ -13,12 +13,8 @@ with h5py.File('./stimulus/nsd_stimuli.hdf5', 'r') as source_hdf5:
     # Loop through and copy the first 1000 images from the source
     for i in range(num_images):
         print(f"Loading image {i+1}/{num_images}")
-        # Read the ith image
-        image_data = source_hdf5['/imgBrick'][0:3, 0:425, 0:425, i]
-        print("Read Image Data")
-
-        # Permute the dimensions to match MATLAB's format
-        image_data = np.transpose(image_data, (2, 1, 0))
+        # Read the ith image in (425, 425, 3) dimension
+        image_data = source_hdf5['/imgBrick'][i, ...]
 
         # Add to the list of images
         images.append(image_data)
