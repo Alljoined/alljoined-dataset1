4a8776456d960cbb7c81a814675373915f997f1e
LemonFace0309 <charles.liu0309@gmail.com>
2024-01-18 22:01:03 -0500

    fix: correct script to get captions

--- scripts/get_coco_labels.py
+++ scripts/get_coco_labels.py
@@ -1,15 +1,24 @@
 import csv
 import json
 
-# Read the first 1000 rows from the CSV file
+# Read the 1000 shared nsd ids from the json file and get additional info from the csv file
+with open('data/coco_indices.json', 'r') as file:
+    coco_idx = json.load(file)
+
 csv_file_path = 'data/nsd_stim_info_merged.csv'
 data = []
 with open(csv_file_path, 'r') as file:
-    reader = csv.DictReader(file)
-    for i, row in enumerate(reader):
-        if i >= 1000:
-            break
-        data.append({'nsdId': row['nsdId'], 'cocoId': row['cocoId'], 'cocoSplit': row['cocoSplit']})
+    for i, idx in enumerate(coco_idx):
+        print(f"Reading index {idx}. Progress: {i+1}/{len(coco_idx)}")
+        isFound = False
+        for row in csv.DictReader(file):
+            if row['nsdId'] == str(idx-1):
+                data.append({'nsdId': row['nsdId'], 'cocoId': row['cocoId'], 'cocoSplit': row['cocoSplit']})
+                isFound = True
+                break
+        if not isFound:
+            print(f"Index {idx} not found in csv file.")
+        file.seek(0)
 
 # Read the JSON files
 with open('data/annotations/captions_train2017.json', 'r') as file:
