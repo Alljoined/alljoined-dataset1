11f66ca5be076c73921cbf93767b3278eff5a5eb
LemonFace0309 <charles.liu0309@gmail.com>
2024-02-21 23:06:27 -0800

    ajskdlfjasf

--- convert_224_stimuli_v2.py
+++ convert_224_stimuli_v2.py
@@ -7,8 +7,8 @@ from scipy.io import savemat, loadmat
 import argparse
 import os
 
-file_path = r"C:\srv\stimulus\stimulus\datasets--pscotti--mindeyev2\snapshots\9b356f8332f385c4256a3a342fff9be3df4ef275\coco_images_224_float16.hdf5"
-label_path = r"C:\srv\stimulus\stimulus\nsd_expdesign.mat"
+file_path = os.path.join("stimulus", "coco_images_224_float16.hdf5")
+label_path = os.path.join("stimulus", "nsd_expdesign.mat")
 save_path = "processed-stimulus"
 #
 # parser = argparse.ArgumentParser(description="Subject # between 1 and 8.")
@@ -20,20 +20,23 @@ save_path = "processed-stimulus"
 #
 # # Parse the arguments
 # args = parser.parse_args()
-args = {}
+args = {
+    "subject": 0,
+    "session": 0,
+}
 
 for sub in range(1, 9):
     for ses in range(1, 21):
-        args.subject = sub
-        args.session = ses
+        args['subject'] = sub
+        args['session'] = ses
 
         # Get image indexes
         mat_contents = loadmat(label_path)
         indices = []
-        if args.subject:
-            indices = mat_contents['subjectim'][args.subject-1]
-            if args.session:
-                split_start = 1000 * ((args.subject - 1) // 2)
+        if args['subject']:
+            indices = mat_contents['subjectim'][args['subject']-1]
+            if args['session']:
+                split_start = 1000 * ((args['subject'] - 1) // 2)
                 indices = indices[split_start:split_start+1000]
         else:
             indices = mat_contents['sharedix'][0]
@@ -59,8 +62,8 @@ for sub in range(1, 9):
         mat_data = {'coco_file': cell_array}
 
         # Save the data to a .mat file
-        name = f"coco_file_224_sub{args.subject}.mat" if args.subject else "coco_file_224_shared.mat"
-        name = f"{name.split(".")[0]}_ses{args.session}.mat" if args.session else name
+        name = f"coco_file_224_sub{args['subject']}.mat" if args['subject'] else "coco_file_224_shared.mat"
+        name = f"{name.split('.')[0]}_ses{args['session']}.mat" if args['session'] else name
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
