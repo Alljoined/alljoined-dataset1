28aa4d2d392c2089b0df04c9539b46f92d35c56e
Michael Ahedor <58152066+mahedor@users.noreply.github.com>
2024-04-29 09:23:25 -0700

    New python stimulus script (#3)
    * New python stimulus script
    
    * Removed redundant instruction text, added in completion text at very end of experiment, images now load BEFORE the first text prompt to avoid delay after pressing space bar at start of first block
    
    * fix bugs: remove shuffling at beginning, added back escape screen, validate oddball ordering, removed unused functions, added back comments, calculating oddball method, consolidated conflicting functions
    
    * small changes to text
    
    ---------
    
    Co-authored-by: mka64 <mka64@cornell.edu>
    Co-authored-by: LemonFace0309 <charles.liu0309@gmail.com>

--- ORE_EEG_2.py
+++ ORE_EEG_2.py
@@ -7,31 +7,36 @@ from tempfile import NamedTemporaryFile
 import random
 
 # Placeholder function for EEG setup and trigger recording
+
+
 def setup_eeg():
     # Initialize EEG, e.g., with Emotiv SDK
     # This function needs to be implemented based on your EEG SDK's documentation
     pass
 
+
 def record_trigger(trigger_number, debug_mode=True):
     # Record or send a trigger to the EEG device
     # This part needs to be adapted based on your EEG device's API
     if debug_mode:
-        logging.log(level=logging.DATA, msg=f"Trigger recorded: {trigger_number}")
+        logging.log(level=logging.DATA,
+                    msg=f"Trigger recorded: {trigger_number}")
     else:
-        pass # Implement actual trigger recording here
+        pass  # Implement actual trigger recording here
+
 
 def validate_block(block_trials):
-    prev  = -1
+    prev = -1
     for trial in block_trials:
         if trial == prev:
             return False
         prev = trial
     return True
 
-# Create trials for the experiment
+
 def create_trials(n_images, n_oddballs, num_blocks, img_width, img_height, window_size):
     trials = []
-    num_block_repeats = num_blocks // 8 # 8 unique blocks in a row
+    num_block_repeats = num_blocks // 8  # 8 unique blocks in a row
 
     for repeat in range(num_block_repeats):
         for block in range(8):
@@ -39,24 +44,26 @@ def create_trials(n_images, n_oddballs, num_blocks, img_width, img_height, windo
             block_trials = []
             while not isValidBlock:
                 # Generate trials for each block
-                start = block * n_images + 1
+                start = 1
                 end = start + n_images
-                images = list(range(start, end)) * 2  # Example: two repeats, adapt as needed
+                # Example: two repeats, adapt as needed
+                images = list(range(start, end)) * 2
                 oddballs = [-1] * n_oddballs
                 block_trials = images + oddballs
                 random.shuffle(block_trials)
                 # ensure no two consecutive trials are the same
                 isValidBlock = validate_block(block_trials)
 
-            for trial in block_trials:
-                trials.append({'block': (block + 1) * repeat, 'trial': trial})
+            for idx, trial in enumerate(block_trials):
+                trials.append({'block': (block + 1) * (repeat + 1), 'trial': trial,
+                              'end_of_block': (idx == len(block_trials) - 1)})
 
     return trials
 
 
 def load_images_from_mat(subj, session_number):
     # Load the .mat file containing the images.
-    # The parameter 'simplify_cells=True' makes nested structures in the .mat file 
+    # The parameter 'simplify_cells=True' makes nested structures in the .mat file
     # easier to access by converting them into nested dictionaries or arrays.
     # This is particularly useful for MATLAB cell arrays and structures,
     # allowing for more Pythonic access to the data.
@@ -64,7 +71,7 @@ def load_images_from_mat(subj, session_number):
     # If you encounter an error with this parameter, ensure you are using a compatible version of SciPy,
     # or you may need to manually navigate the nested structures without this parameter.
     filename = f'processed-stimulus/coco_file_224_sub{subj}_ses{session_number}.mat'
-    loaded_data = loadmat(filename, simplify_cells=True)['coco_file']  
+    loaded_data = loadmat(filename, simplify_cells=True)['coco_file']
     images = []
 
     # Iterate through each item in the loaded image data.
@@ -72,7 +79,6 @@ def load_images_from_mat(subj, session_number):
         # Access the image data. Given the structure noted, each 'img_data' should be directly
         # an RGB image with the shape (224, 224, 3), meaning no additional reshaping or squeezing is needed.
         img_data = loaded_data[i]
-
         # Ensure the image data is in the expected uint8 format for image processing.
         # This step converts the MATLAB image data into a format suitable for creating an image file.
         img_array = np.uint8(img_data)
@@ -83,31 +89,82 @@ def load_images_from_mat(subj, session_number):
         # allowing us to use the file path for display in PsychoPy.
         with NamedTemporaryFile(delete=False, suffix='.png') as tmp:
             Image.fromarray(img_array).save(tmp.name)
-            images.append(tmp.name)  # Save the path to the temporary file for later use.
+            # Save the path to the temporary file for later use.
+            images.append(tmp.name)
 
+    # Print the total number of images
+    print(f"\nTotal number of images loaded: {len(images)} \n")
     return images
 
 
-# Main experiment
-def run_experiment(trials, window, subj, session_number):
-    stim_images = load_images_from_mat(subj, session_number)
+def select_block_images(all_images, block_number, n_images):
+    # Mod operator is used to ensure that blocks 9-16 are the same as block 1-8
+    start_index = ((block_number - 1) % 8) * n_images
+    end_index = start_index + n_images
+    return all_images[start_index:end_index]
 
+
+def display_instructions(window, session_number):
+    instruction_text = (
+        f"Welcome to block {session_number} of the study.\n\n"
+        "In this session, you will complete a perception task.\n"
+        "This session consists of 16 experimental blocks.\n\n"
+        "You will see sequences of images appearing on the screen, your task is to "
+        "press the space bar when you see an image appear twice in a row.\n\n"
+        "When you are ready, press the space bar to start."
+    )
+
+    # Assuming a window width of 800 pixels, adjust this based on your actual window size
+    # Use 80% of window width for text wrapping
+    wrap_width = window.size[0] * 0.8
+
+    message = visual.TextStim(window, text=instruction_text, pos=(
+        0, 0), color=(1, 1, 1), height=40, wrapWidth=wrap_width)
+    message.draw()
+    window.flip()
+    event.waitKeys(keyList=['space'])
+
+
+def run_experiment(trials, window, subj, session_number, n_images, all_images):
     last_image = None
-    for trial in trials:
+
+    current_block = 1  # Initialize the current block counter
+    # Initialize an empty list to hold the image numbers for the current block
+    image_sequence = []
+
+    for idx, trial in enumerate(trials):
         if 'escape' in event.getKeys():
             print("Experiment terminated early.")
             break
 
-        if trial['trial'] == -1 and last_image is not None:
-            # Oddball trial, display the last image again
+        if trial['block'] != current_block:
+            current_block = trial['block']
+            start_index = ((current_block - 1) % 8) * n_images
+            end_index = start_index + n_images
+            print(f"\nBlock {current_block}, Start Index: {start_index}")
+            print(f"Block {current_block}, End Index: {end_index}\n")
+
+        block_images = select_block_images(
+            all_images, trial['block'], n_images)
+        # Adjust index for 0-based Python indexing
+        image_path = block_images[trial['trial'] - 1]
+        # Check if this trial is an oddball
+        is_oddball = (trial['trial'] == -1)
+        if is_oddball:
             image_path = last_image
         else:
-            # Regular trial
-            image_path = stim_images[trial['trial'] - 1]  # Adjust index for 0-based
             last_image = image_path
-        
+
+        # Append current image number to the sequence list
+        image_sequence.append(trial['trial'])
+
+        # Logging the trial details
+        print(
+            f"Block {trial['block']}, Trial {idx + 1}: Image {trial['trial']} {'(Oddball)' if is_oddball else ''}")
+
         # Display the image
-        image_stim = visual.ImageStim(win=window, image=image_path, pos=(0, 0), size=(448, 448))
+        image_stim = visual.ImageStim(
+            win=window, image=image_path, pos=(0, 0), size=(448, 448))
         image_stim.draw()
         window.flip()
         core.wait(0.3)  # Display time
@@ -118,30 +175,54 @@ def run_experiment(trials, window, subj, session_number):
         # Record a placeholder trigger
         record_trigger(99)
 
-        # Block end message and wait for space press to continue, if end of block
-        if trial.get('end_of_block', False):
-            display_block_end_message(window, trial['block'], len(trials) // len(trial['block']))
+        # Check if end of block
+        if trial['end_of_block']:
+            # Print the image sequence for the current block
+            print(
+                f"\nEnd of Block {trial['block']} Image Sequence: \n {', '.join(map(str, image_sequence))}")
+            # Clear the list for the next block
+            image_sequence = []
+
+            # Display break message at the end of each block
+            display_break_message(window, trial['block'])
+
+    # Display completion message
+    display_completion_message(window)
 
     # Cleanup: Remove temporary image files
-    for img_path in stim_images:
+    for img_path in all_images:
         os.remove(img_path)
-    
+
     window.close()
     core.quit()
 
+
+def display_break_message(window, block_number):
+    message = f"You've completed block {block_number}.\n\nTake a little break and press the space bar when you're ready to continue to the next block."
+    break_message = visual.TextStim(window, text=message, pos=(
+        0, 0), color=(1, 1, 1), height=40, wrapWidth=window.size[0] * 0.8)
+    break_message.draw()
+    window.flip()
+    event.waitKeys(keyList=['space'])
+
+
+def display_completion_message(window):
+    completion_text = "Congratulations! You have completed the experiment.\n\nPress the space bar to exit."
+    completion_message = visual.TextStim(window, text=completion_text, pos=(
+        0, 0), color=(1, 1, 1), height=40, wrapWidth=window.size[0] * 0.8)
+    completion_message.draw()
+    window.flip()
+    event.waitKeys(keyList=['space'])
+
+
 def display_cross_with_jitter(window, base_time, jitter):
     rest_period = base_time + random.randint(0, int(jitter * 100)) / 100.0
-    fixation_cross = visual.TextStim(window, text='+', pos=(0, 0), color=(1, 1, 1))
+    fixation_cross = visual.TextStim(
+        window, text='+', pos=(0, 0), color=(1, 1, 1), height=40)
     fixation_cross.draw()
     window.flip()
     core.wait(rest_period)
 
-def display_block_end_message(window, block_number, total_blocks):
-    message = f"You have completed {block_number} out of {total_blocks} blocks.\nTake a short rest\nPress Space to continue"
-    block_message = visual.TextStim(window, text=message, pos=(0, 0), color=(1, 1, 1))
-    block_message.draw()
-    window.flip()
-    event.waitKeys(keyList=['space'])
 
 def main():
     # Experiment setup
@@ -152,7 +233,13 @@ def main():
 
     # Setup window
     window = visual.Window(fullscr=True, color=[0, 0, 0], units='pix')
-    window_size = window.size
+
+    # Load and shuffle images before displaying instructions
+    all_images = load_images_from_mat(
+        participant_info['Subject'], participant_info['Session'])
+
+    # Display instructions
+    display_instructions(window, participant_info['Session'])
 
     # Setup EEG
     setup_eeg()
@@ -161,16 +248,20 @@ def main():
     n_images = 120  # Number of unique images
     n_oddballs = 24  # Number of oddball images
     num_blocks = 16  # Number of blocks
-    img_width, img_height = 425, 425
+    img_width, img_height = 425, 425  # Define image dimensions
+    window_size = window.size
 
-    trials = create_trials(n_images, n_oddballs, num_blocks, img_width, img_height, window_size)
+    trials = create_trials(n_images, n_oddballs, num_blocks,
+                           img_width, img_height, window_size)
 
     # Run the experiment
-    run_experiment(trials, window, participant_info['Subject'], participant_info['Session'])
+    run_experiment(
+        trials, window, participant_info['Subject'], participant_info['Session'], n_images, all_images)
 
     # Save results
     # This is where you would implement saving the collected data
     # e.g., response times, accuracy, etc., to a file
+
 
 if __name__ == '__main__':
     main()
